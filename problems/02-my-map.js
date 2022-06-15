1441848ccd211973f750a40b5561cf2a6f09fcb7
Laura Gonzales <laurevi.gonzales@gmail.com>
2022-06-15 11:46:51 -0700

    problem 1 solved, problem 2 in progress

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,8 +16,10 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
-
+let myMap = function(arr, cb) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++){
+    }
 };
 
 
